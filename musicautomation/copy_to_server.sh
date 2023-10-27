efb63df20f1701cc5e91eb48cd79093a6c33a698
rhysbratti <148360588+rhysbratti@users.noreply.github.com>
2023-10-27 15:15:51 -0400

    Updated to newer version of copy_to_server.sh

--- musicautomation/copy_to_server.sh
+++ musicautomation/copy_to_server.sh
@@ -3,10 +3,7 @@ remote_download_path="~/downloads/"
 remote_host="user-here@123.456.7.89"
 remote_command="bash ~/scripts/musicautomation/import_music_files.sh"
 
-for i in $local_download_path/*.zip; do
-    [ -f "$i" ] || break
-    rsync $i $remote_host:$remote_download_path
-    rm -rf $i
-done
+rsync $local_download_path/*.zip $remote_host:$remote_download_path
+rm -rf $local_download_path/*.zip
 
 ssh $remote_host $remote_command
